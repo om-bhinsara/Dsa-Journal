6f1ad7e273027189774f42f557ffeb8ee9d01b30
Bhisra om <156498417+om-bhinsara@users.noreply.github.com>
2025-08-01 06:37:32 +0530

    Add files via upload

---
+++ 01) Pattern Printing/Lec 08/Pattern 3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std; 
+
+int main()
+{
+    cout<<"Enter N: "; 
+    int N; 
+    cin>>N; 
+
+    for(int row=1; row<=N; row++)
+    {
+        for(int col=1; col<=(N-row); col++)
+        {
+            cout<<"  "; 
+        }
+        for(int col=1; col<=row; col++)
+        {
+            cout<<col<<" "; 
+        }
+        cout<<endl;
+    }
+    return 0; 
+}
